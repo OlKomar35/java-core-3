6619218ccb426d0c86558aa626c067f4bb3cee98
olga_komar <komar_ol@mail.ru>
2024-03-25 09:40:45 +0300

    готовое дз

--- src/main/java/org/komar/Calculator.java
+++ src/main/java/org/komar/Calculator.java
@@ -1,4 +1,22 @@
 package org.komar;
 
 public class Calculator {
+
+    public static <T extends Number> double sum(T a, T b) {
+        return a.doubleValue() + b.doubleValue();
+    }
+
+    public static <T extends Number> double multiply(T a, T b) {
+        return a.doubleValue() * b.doubleValue();
+    }
+
+    public static <T extends Number> double divide(T a, T b) {
+        return a.doubleValue() / b.doubleValue();
+    }
+
+    public static <T extends Number> double subtract(T a, T b) {
+        return a.doubleValue() - b.doubleValue();
+    }
 }
+
+
